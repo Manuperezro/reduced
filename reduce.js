363f169131dbea037c4e03c2acdc1fda5a73a0c2
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-01-15 13:10:09 +0000

    learning

--- reduce.js
+++ reduce.js
@@ -7,6 +7,16 @@
 
 // Summing an array of numbers:
 
+nums = [0, 1,2,3,4,];
+ let sum = nums.reduce((acc, curr) => { 
+   console.log(
+     'Accumulator:', acc,
+     'Current value:', curr,
+     'Total:', acc + curr
+   );
+   return acc + curr;
+
+   }, 35);
 
 const teamMembers = [
   {
@@ -33,5 +43,19 @@ const teamMembers = [
 
 // Totaling a specific object property
 
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0); 
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+
+let experienceByProfession = teamMembers.reduce((acc, curr) => { 
+  let key = curr.profession; 
+  if (!acc[key]){
+    acc[key] = curr.yrsExperience;
+  } else { 
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(experienceByProfession);
